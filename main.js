639f8d6ae1ae3a9c9f1ea9738dd6afae5a5e4381
DulceBucio <114613877+DulceBucio@users.noreply.github.com>
2023-07-18 14:19:19 -0600

    updated images and screenshots

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let typewriter = new Typewriter(app, {
  
 typewriter
   .pauseFor(2500)
-  .typeString('Coding apassionate and trying to spread STEM knowledge')
+  .typeString('Unleashing Coding Passion, Empowering STEM Knowledge')
   .pauseFor(200)
   .deleteChars(10)
   .start();
